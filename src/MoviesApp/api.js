659dd2bca662a3b9e48b22af080872a2e0e8cc70
Are Sathvika <sathvikaare27@gmail.com>
2024-10-05 14:25:19 +0530

    removed env file

--- src/MoviesApp/api.js
+++ src/MoviesApp/api.js
@@ -1,14 +1,15 @@
-// const API_KEY = '8bcb42fc';
-// const API_URL = 'http://www.omdbapi.com/';
+const API_KEY = '8bcb42fc';
+const API_URL = 'https://www.omdbapi.com/';
 
+// console.log(API_URL)
 export const fetchMovies = async (query) => {
-  const response = await fetch(`${process.env.API_URL}?s=${query}&apiKey=${process.env.API_KEY}`);
+  const response = await fetch(`${API_URL}?s=${query}&apiKey=${API_KEY}`);
   const data = await response.json();
   return data.Search;
 };
 
 export const fetchMovieDetails = async (id) => {
-  const response = await fetch(`${process.env.API_URL}?i=${id}&apiKey=${process.env.API_KEY}`);
+  const response = await fetch(`${API_URL}?i=${id}&apiKey=${API_KEY}`);
   const data = await response.json();
   return data;
 };
